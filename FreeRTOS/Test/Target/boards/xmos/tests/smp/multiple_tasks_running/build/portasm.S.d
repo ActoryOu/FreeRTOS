643917a47234531252be51e119f6c8c10e6008df
ActoryOu <ousc@amazon.com>
2023-02-22 11:13:11 +0800

    Remove build binaries.

--- FreeRTOS/Test/Target/boards/xmos/tests/smp/multiple_tasks_running/build/portasm.S.d
+++
@@ -1,8 +0,0 @@
-build/portasm.S.o build/portasm.S.d: \
-  ../../../../../../../Source/portable/ThirdParty/xClang/XCOREAI/portasm.S \
-  C:/git/ActoryForked/FreeRTOS/FreeRTOS/Source/portable/ThirdParty/xClang/XCOREAI/rtos_support_rtos_config.h
-
-../../../../../../../Source/portable/ThirdParty/xClang/XCOREAI/portasm.S:
-
-C:/git/ActoryForked/FreeRTOS/FreeRTOS/Source/portable/ThirdParty/xClang/XCOREAI/rtos_support_rtos_config.h:
-
--- FreeRTOS/Test/Target/boards/xmos/tests/smp/multiple_tasks_running/build/unity.c.d
+++
@@ -1,8 +0,0 @@
-build/unity.c.o build/unity.c.d: \
-  ../../../../../../CMock/CMock/vendor/unity/src/unity.c \
-  ../../../../../../CMock/CMock/vendor/unity/src/unity.h \
-  ../../../../../../CMock/CMock/vendor/unity/src/unity_internals.h
-
-../../../../../../CMock/CMock/vendor/unity/src/unity.h:
-
-../../../../../../CMock/CMock/vendor/unity/src/unity_internals.h:
